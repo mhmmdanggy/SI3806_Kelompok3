5fdb005f9b3e9a5b4545a5621a4240ce88e47fa3
monyonk <ariapridana@gmail.com>
2017-03-11 23:03:43 +0700

    buat activity forgot password

---
+++ app/src/main/java/com/example/indovent/SI3806_Kelompok3/forgot_password.java
@@ -0,0 +1,12 @@
+package com.example.indovent.SI3806_Kelompok3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class forgot_password extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
